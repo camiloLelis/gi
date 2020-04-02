93e01c7d9eca4326d5a1a189af90aabc8fa0bdc5
camiloLelis <lafirmafuerte@gmail.com>
2020-04-02 01:15:40 -0300

    transformar dias em anos

--- idade2.py
+++ idade2.py
@@ -8,7 +8,9 @@ nasceu= datetime.datetime(int(result[0]), int(result[1]), int(result[2])) # aqui
 print (result)
 print (nasceu)
 data= datetime.datetime.now()# aqui joga o return do metodo datetime.datetime.now() que é o dia mes e ano e horas atual para variavel data
-idade= data-nasceu #aqui divide a data atual que tá na variavel data pelo variavel que mostra o dia que nasceu, e joga na variavel idade
-idcerta=re.sub(r'days,','dias que viveu', str(idade)) #usei o metodo re.sub() que substitui o 1º texto do 1ºparametro pelo 2ºsegundo , no que estiver no 3ºparametro
-print(idcerta)
+idade= data-nasceu #aqui divide a data atual que1 tá na variavel data pelo variavel que mostra o dia que nasceu, e joga na variavel idade
+idcerta=int(re.sub(r'days,...............','', str(idade))) #usei o metodo re.sub() que substitui o 1º texto do 1ºparametro pelo 2ºsegundo , no que estiver no 3ºparametro
+print(str(idcerta) + " dias que você viveu até hoje ")
+anos= int(int(idcerta) / 365.25) # transforma a variavel idcerta de string em inteiro e divide pelo numeros de dias de um ano,coloquei um int a mais para delimitar o numero de casas decimais, ou seja não dar numeros quebrados,365,25 devido a ano bisexto, mais não pensei direito 
+print ("você tem " + str(anos) + " anos de vida")
 #print(idade)
